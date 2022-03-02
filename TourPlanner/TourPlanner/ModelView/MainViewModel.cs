5c7ef9499028128f53c7ea4acba74704ad085d4c
mucki024 <mucki024.0@gmail.com>
2022-03-02 20:15:55 +0100

    added first classes

---
+++ TourPlanner/TourPlanner/ModelView/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace TourPlanner
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Tour> Data { get; } = new ObservableCollection<Tour>();
+        public RelayCommand AddCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            /*
+            AddCommand = new RelayCommand((_) =>
+            {
+                Data.Add(new HighscoreEntry(this.CurrentUsername, this.CurrentPoints));
+                CurrentUsername = string.Empty;
+                CurrentPoints = string.Empty;
+                OnPropertyChanged(nameof(CurrentUsername));
+                OnPropertyChanged("CurrentPoints");
+                IsUsernameFocused = true;
+            });
+            IsUsernameFocused = true;
+            */
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Debug.Print($"propertyChanged \"{propertyName}\"");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
