180c6141f9e3aaa53e9ce27fababcf7a8aca3e47
mucki024 <mucki024.0@gmail.com>
2022-03-09 17:00:33 +0100

    added Tourbutton logic

--- TourPlanner/TourPlanner/ModelView/MainViewModel.cs
+++ TourPlanner/TourPlanner/ModelView/MainViewModel.cs
@@ -5,27 +5,44 @@ using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using TourPlanner.Model;
 using TourPlanner.BusinessLayer;
+using System.Drawing.Printing;
+using Aspose.Cells.Drawing;
+using System.Windows;
 
 namespace TourPlanner
 {
     public class MainViewModel : ViewModelBase 
     {
         private ITourFactory _tourfactory;
+
         public ObservableCollection<Tour> TourData { get; } = new ObservableCollection<Tour>();
-        
-        /* TODO: implement selected Tour behaivor
-        private bool _selectedTour;
-        public bool SelectedTour
+        private MouseButtonEventHandler _changebaleTour;
+        public MouseButtonEventHandler ChangebaleTour
+        {
+            get => _changebaleTour;
+            set
+            {   //set selected Tour and update UI
+                _changebaleTour = value;
+                OnPropertyChanged(nameof(ChangebaleTour));
+            }
+        }
+
+        private Tour _selectedTour;
+        public Tour SelectedTour
         {
             get => _selectedTour;
             set
             {   //set selected Tour and update UI
+                if (_selectedTour == value)
+                    Debug.Print("Clicked twice");
                 _selectedTour = value;
-                OnPropertyChanged(nameof(SelectedTour);
+                //OnPropertyChanged(nameof(SelectedTour));
             }
-        }*/
+        }
         public RelayCommand ExecuteTextSearch { get; }
         public RelayCommand AddTour { get; }
+        public RelayCommand DeleteTour { get; }
+        public RelayCommand ChangeTour { get; }
 
         public MainViewModel()
         {
@@ -42,10 +59,27 @@ namespace TourPlanner
             */
             AddTour = new RelayCommand((_) =>
             {
+                Window window = new Window(); // wrong window 
+                window.Height = 400;
+                window.Width = 300;
+                window.ShowDialog();
+
+                //MessageBox.Show("Your request will be processed.");
                 TourData.Add(new Tour("DummyTour"));
-                //OnPropertyChanged()
             });
-            
+
+            DeleteTour = new RelayCommand((_) =>
+            {
+                if (SelectedTour != null) 
+                    TourData.Remove(SelectedTour);
+            });
+
+            ChangeTour = new RelayCommand((_) =>
+            {
+                //if (SelectedTour != null)
+                    
+            });
+
         }
 
     }
---
+++ TourPlanner/TourPlannerTest/TourPlannerTest/MainViewModelTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using TourPlanner;
+
+namespace TourPlannerTest
+{
+    public class MainViewModelTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            MainViewModel model = new MainViewModel();
+            Assert.Pass();
+        }
+
+        //test behaviour for add button 
+    }
+}
