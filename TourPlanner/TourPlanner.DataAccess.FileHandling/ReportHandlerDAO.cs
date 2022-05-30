e60cc245408309f710a57ea1d0d4868899d25bf4
Philip <if20b076@technikum-wien.at>
2022-05-30 15:00:10 +0200

    Added MultiExport for Report
    Added MultiExport for Report

--- TourPlanner/TourPlanner.DataAccess.FileHandling/ReportHandlerDAO.cs
+++ TourPlanner/TourPlanner.DataAccess.FileHandling/ReportHandlerDAO.cs
@@ -25,7 +25,7 @@ namespace TourPlanner.DataAccess.FileHandling
                 Document document = new Document(pdf);
                 Paragraph TourNameHeader = new Paragraph(tourModel.Tourname)
                             .SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA))
-                            .SetFontSize(14)
+                            .SetFontSize(16)
                             .SetBold();
                 Paragraph TourData = new Paragraph("From: "+tourModel.Start+"\n"+"To: "+tourModel.Destination+"\n" + tourModel.RouteInformation)
                                .SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA));
@@ -54,9 +54,34 @@ namespace TourPlanner.DataAccess.FileHandling
             throw new NotImplementedException();
         }
 
-        public Task<bool> MultiExport(IEnumerable<Tour> tourModels, string path)
-        {
-            throw new NotImplementedException();
+        public async Task<bool> MultiExport(IEnumerable<Tour> tourModels, string path) 
+        {//should get called after full db querry to make sure all tours are supplied
+            await Task.Run(() =>
+            {
+                string fileName = path + "\\TourReport.pdf";
+                PdfWriter writer = new PdfWriter(fileName);
+                PdfDocument pdf = new PdfDocument(writer);
+                Document document = new Document(pdf);
+                Paragraph Header = new Paragraph("Available Tours")
+                            .SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA))
+                            .SetFontSize(16)
+                            .SetBold();
+                List Tourlist = new List()
+                      .SetSymbolIndent(12)
+                      .SetListSymbol(" ")
+                      .SetFont(PdfFontFactory.CreateFont(StandardFonts.TIMES_BOLD));
+                foreach (Tour tour in tourModels)
+                {
+                    Tourlist.Add(new ListItem(tour.TourID + ": " + tour.Tourname))
+                            .Add(new ListItem(tour.Start + " - " + tour.Destination))
+                            .Add(new ListItem(tour.RouteInformation))
+                            .Add(new ListItem(""));
+
+                }
+                document.Add(Tourlist);
+                document.Close();
+            });
+            return true;
         }
     }
 }
