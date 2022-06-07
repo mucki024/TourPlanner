205217215abf2380440ebc40e6a8c82ba468a6c4
mucki024 <mucki024.0@gmail.com>
2022-06-07 18:13:12 +0200

    additionalUnitTest

--- TourPlanner/TourPlannerTest/TourPlannerTest/MainViewModelTest.cs
+++ TourPlanner/TourPlannerTest/TourPlannerTest/MainViewModelTest.cs
@@ -1,6 +1,9 @@
 using NUnit.Framework;
 using TourPlanner;
 using Moq;
+using TourPlanner.BusinessLayer;
+using TourPlanner.Model;
+using System.Collections.Generic;
 
 namespace TourPlannerTest
 {
@@ -11,16 +14,22 @@ namespace TourPlannerTest
         {
         }
 
-        /*
+        
         [Test]
-        public void Test1()
+        public void MainViewModel_CheckTourCount()
         {
-            var moqFactory = new Mock<IWindowFactory>();
-            var moqSubViewModel = new Mock<SubWindowViewTour>();
-            MainViewModel model = new MainViewModel(moqFactory.Object, moqSubViewModel.Object);
-            Assert.IsTrue(model.TourData.Count == 2);
+            var moqFactory = new Mock<WindowFactory>();
+            var moqSubWindowTour = new Mock<SubWindowViewTour>();
+            var moqSubViewTourDescription= new Mock<SubViewTourDescription>();
+            var moqSubSubwindowViewLog= new Mock<SubWindowViewLog>();
+            ITourFactory fac = TourFactory.GetInstance();
+            var moqSubViewTourLogs= new SubViewTourLogs(moqSubSubwindowViewLog.Object, moqFactory.Object, fac);
+            List<Tour> tmp = (List<Tour>)fac.getAllTours();
+            
+            MainViewModel model = new MainViewModel(moqFactory.Object, moqSubWindowTour.Object, moqSubViewTourDescription.Object, moqSubViewTourLogs, moqSubSubwindowViewLog.Object);
+
+            Assert.IsTrue(model.TourData.Count == tmp.Count);
         }
-        */
-        //test behaviour for add button 
+
     }
 }
